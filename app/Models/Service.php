59767a55f4596b2b6debd8a669a4a504edefeab5
Debapriyo Ganguly <debapriyoganguly@Debapriyos-MacBook-Air.local>
2026-04-10 05:32:46 +0600

    Added my new feature

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -10,10 +10,16 @@ class Service extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'title',
         'description',
         'category',
         'subcategory',
         'price'
     ];
+
+    public function provider()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
